9ca9eef033518024decf812b95007dd23b466475
dksldhodkseho <mryongju@gmail.com>
2026-01-08 23:10:39 +0900

    [feat] User Document  Define
    poetry add beanie

---
+++ Backend/servers/user/models/user_document.py
@@ -0,0 +1,19 @@
+# Backend\servers\user\models\user_document.py
+# beanie
+
+from datetime import datetime
+from beanie import Document, Field
+
+class User(Document):
+    id: str
+    password: str
+    name: str
+    phone: str
+    birth: datetime
+
+    created_at: datetime = Field(default_factory=datetime.now)
+
+
+    class Settings:
+        name = "users"
+
