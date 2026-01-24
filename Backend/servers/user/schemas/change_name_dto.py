b346bfdc43ff13c19bff85b1eed28656f0e23fd0
dksldhodkseho <mryongju@gmail.com>
2026-01-25 02:34:50 +0900

    [Refactor] Name Change

--- Backend/servers/user/schemas/change_name_dto.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from starlette.responses import JSONResponse
-
-class RequestChangeName(BaseModel):
-    new_name: str
-
-class ResponseChangeName(JSONResponse):
-    status_code: int
-    content: dict
