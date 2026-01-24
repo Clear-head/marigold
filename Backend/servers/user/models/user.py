8e71ea01af732bbdd2442fabf8f6c36d00ae36e9
dksldhodkseho <mryongju@gmail.com>
2026-01-24 16:31:49 +0900

    [feat] User Document Define

---
+++ Backend/servers/user/models/user.py
@@ -0,0 +1,14 @@
+# Backend\servers\user\models\user.py
+
+from beanie import Document, Field
+from datetime import datetime
+
+class User(Document):
+    id: str
+    name: str
+    phone: str
+    birth: datetime
+    created_at: datetime = Field(default_factory=datetime.now)
+
+    class Settings:
+        name = "users"
